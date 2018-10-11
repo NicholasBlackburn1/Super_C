37cc659e45b2713274a15db2061c932efd5ad7c6
BuildTools <unconfigured@null.spigotmc.org>
2018-10-11 10:41:52 -0400

    created loading screen

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-printf("helloworld\n");
-
+void loading();
 return 0;
 }
+
+int loading()
+{
+  int i = 0;
+       for (i = 0; i < 10000; i++){
+           printf("In progress %d", i/100);
+       }
+       printf("\n");
+   }
