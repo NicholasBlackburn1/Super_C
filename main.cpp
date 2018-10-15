e745c8a8fd1310146db605e86e81fad760d4cd60
BuildTools <unconfigured@null.spigotmc.org>
2018-10-15 10:59:38 -0400

    qq

--- main.cpp
+++ main.cpp
@@ -66,6 +66,11 @@ void text()
     }
 void ui()
     {
+        initscr();			/* Start curses mode 		  */
+	    printw("Hello World !!!");	/* Print Hello World		  */
+	    refresh();			/* Print it on to the real screen */
+	    getch();			/* Wait for user input */
+	    endwin();			/* End curses mode		  */
     }
 
     
